37a6342a969094d82db7f14c1290d21681ac80b8
CDarts48 <cdartswebdev@gmail.com>
2025-01-14 20:25:45 -0700

    flags up and over

---
+++ pages/homepage/+Page.jsx
@@ -0,0 +1,12 @@
+function Page() {
+    return (
+        <>
+            <h1>Colorado Films</h1>
+            <p>Showcasing films with Colorado connection.</p>
+            <p>Coming soon...</p>
+            <img src="/flag.png" alt="Colorado Flag" />
+        </>
+    )   
+}
+
+export { Page }
--- renderer/Layout.jsx
+++ renderer/Layout.jsx
@@ -1,30 +1,21 @@
-export { Layout }
-
 import React from 'react'
 import PropTypes from 'prop-types'
-import { childrenPropType } from './PropTypeValues'
-import logoUrl from './logo.svg'
 import { PageContextProvider } from './usePageContext'
-import { Link } from './Link'
+import { Page as HomePage } from '../pages/homepage/+Page'
 import './css/index.css'
 import './Layout.css'
 
 Layout.propTypes = {
   pageContext: PropTypes.any,
-  children: childrenPropType
+  children: PropTypes.node
 }
+
 function Layout({ pageContext, children }) {
   return (
     <React.StrictMode>
       <PageContextProvider pageContext={pageContext}>
         <Frame>
-          <Sidebar>
-            <Logo />
-            <Link href="/">Welcome</Link>
-            <Link href="/about">About</Link>
-            <Link href="/star-wars">Data Fetching</Link>
-          </Sidebar>
-          <Content>{children}</Content>
+          <HomePage />
         </Frame>
       </PageContextProvider>
     </React.StrictMode>
@@ -32,8 +23,9 @@ function Layout({ pageContext, children }) {
 }
 
 Frame.propTypes = {
-  children: childrenPropType
+  children: PropTypes.node
 }
+
 function Frame({ children }) {
   return (
     <div
@@ -48,58 +40,4 @@ function Frame({ children }) {
   )
 }
 
-Sidebar.propTypes = {
-  children: childrenPropType
-}
-function Sidebar({ children }) {
-  return (
-    <div
-      id="sidebar"
-      style={{
-        padding: 20,
-        flexShrink: 0,
-        display: 'flex',
-        flexDirection: 'column',
-        lineHeight: '1.8em',
-        borderRight: '2px solid #eee'
-      }}
-    >
-      {children}
-    </div>
-  )
-}
-
-Content.propTypes = {
-  children: childrenPropType
-}
-function Content({ children }) {
-  return (
-    <div id="page-container">
-      <div
-        id="page-content"
-        style={{
-          padding: 20,
-          paddingBottom: 50,
-          minHeight: '100vh'
-        }}
-      >
-        {children}
-      </div>
-    </div>
-  )
-}
-
-function Logo() {
-  return (
-    <div
-      style={{
-        marginTop: 20,
-        marginBottom: 10
-      }}
-    >
-      <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
-      </a>
-    </div>
-  )
-}
+export { Layout }
