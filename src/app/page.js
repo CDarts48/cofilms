f976c4a2600879caf78630eb8069ce9812421220
Corey Donahue <cdartswebdev@gmail.com>
2025-04-04 10:49:24 -0600

    commented out move slider for now.

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="page">
       <Sidebar/>
       <HeroSection />
-      <MovieSlider />
+      {/* <MovieSlider /> */}
       <ContactSection />
     </div>
   );
