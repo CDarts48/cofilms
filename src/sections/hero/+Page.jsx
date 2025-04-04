234764fb8306abb18dcb5cd1e7c87603bb4f99a5
Corey Donahue <cdartswebdev@gmail.com>
2025-04-04 10:49:24 -0600

    commented out move slider for now.

--- src/sections/hero/+Page.jsx
+++ src/sections/hero/+Page.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 import './code.css';
 
 function HeroSection() {
-    return (
-        <div className="hero-section">
-        </div>
-    );
+  return (
+    <section
+      className="hero-section"
+      style={{
+        background: "url('/Peaks.png') center/100% 100% no-repeat",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "top",
+      }}
+    >
+      <div className="hero-content">
+        <h1 className="hero-title">Colorado Films</h1>
+        <p className="hero-description">
+        Colorado Film is your gateway to discovering movies shaped by the spirit of Colorado.</p>
+        <a href="#explore" className="explore-button">
+          Explore Now
+        </a>
+      </div>
+    </section>
+  );
 }
 
 export default HeroSection;
