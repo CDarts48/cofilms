c27bbb53665e41428965f2a2ac2f8dcce93b40dc
Corey Donahue <cdartswebdev@gmail.com>
2025-07-28 17:33:14 -0600

    more

--- src/app/film/slider/Slider.jsx
+++ src/app/film/slider/Slider.jsx
@@ -63,7 +63,6 @@ export default function MovieSlider() {
     );
   }
 
-  // Inline styles
   const containerStyle = {
     maxWidth: 520,
     margin: '40px auto',
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import React from 'react';
 import HeroSection from '../sections/hero/+Page';
 import ContactSection from '../sections/ContactSection';
 import Header from '../sections/header/Header'; 
+import MovieSlider from './film/slider/Slider';
 
 export default function Home() {
   return (
     <div className="page">
       <Header />
+      <MovieSlider /> 
       <HeroSection />
       <ContactSection />
     </div>
