68c95d24197ba19e2741565beeb03d46ef08bb75
Corey Donahue <corey@cdartswebdev.com>
2025-11-11 16:24:50 -0700

    more migration

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Metadata } from "next";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Colorado Films",
+    description: "Exploring Colorado's Entertainment Industry",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}): React.ReactElement {
+    return (
+        <html lang="en">
+            <body className={`${geistSans.variable} ${geistMono.variable}`}>
+                {children}
+            </body>
+        </html>
+    );
+}
