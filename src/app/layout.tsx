acad8b2836a80690f1a5d50b6526cf658be42f02
Corey Donahue <corey@cdartswebdev.com>
2025-11-30 20:53:40 -0700

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,11 @@ export const metadata: Metadata = {
     icons: {
         icon: '/favicon.png',
     },
+    viewport: {
+        width: 'device-width',
+        initialScale: 1,
+        maximumScale: 5,
+    },
 };
 
 export default function RootLayout({
