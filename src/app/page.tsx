5e16faf5cd2d09f9fbfd88f15381125e9ba39c6a
Corey Donahue <corey@cdartswebdev.com>
2025-12-20 18:09:32 -0700

    update header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BasicHeader from '../components/BasicHeader';
 import HeroSection from '../sections/hero/+Page';
 import ContactSection from '../components/ContactSection';
 import PodCastSection from '../sections/PodCastSection';
@@ -7,6 +8,7 @@ import UpcomingFilmFestivals from '../sections/UpcomingFilmFestivals';
 export default function Home(): React.ReactElement {
   return (
     <div className="page">
+      <BasicHeader />
       <HeroSection />
       <PodCastSection />
       <UpcomingFilmFestivals />
